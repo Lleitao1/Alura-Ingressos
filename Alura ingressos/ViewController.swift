4ef008e5e194f5ca5621a32c85012674ee1250f6
Lleitao1 <lucas.leitao25@gmail.com>
2021-01-06 11:53:33 -0300

    primeiro commit

---
+++ Alura ingressos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alura ingressos
+//
+//  Created by Lucas Abdel Leitao on 06/01/21.
+//  Copyright © 2021 Lucas Abdel Leitao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
